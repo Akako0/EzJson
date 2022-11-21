711050f2c9d89661faf9d047644a2ac2f8cddcb4
Akakø <Github@louis.guerrier.com>
2022-11-22 00:44:10 +0100

    1.2
    Added encoding support

--- ez_json.py
+++ ez_json.py
@@ -1,23 +1,41 @@
+"""
+Ver: 0.1.2
+Credit: Akako0 https://github.com/Akako0/ez_json
+This code is open source and can be modified as wanted.
+
+Ez_json is a simple json library that allows you to easily create, read and modify json files.
+It is not a replacement for the json module, but a wrapper for it. 
+It is not as fast as the json module, but it is much easier to use.
+The main goal of this module is to make json files as easy to use as possible not to replace the json module.
+If you have any suggestions or find any bugs, please let me know.
+If you want to support this project, please give it a star on github.
+Check also my buy me a coffee page: https://www.buymeacoffee.com/akakoTheDev
+"""
+
 from json import load, dump
 from pathlib import Path
 
-class Json:
+class EzJson:
     """
     Makes json easier to use in python
     Arguments: file_path: str
     """
-    def __init__ (self, file_path: str) -> None:
+    def __init__ (self, file_path: str, encoding: str = 'utf-8') -> None:
         self.file_path = file_path
+        self.encoding = encoding
         self.exists = Path(file_path).exists()
         self.file_name = Path(file_path).name
+        if not self.exists:
+            self.empty = True
+            self.file_size = 0
+            print(
+                f"Warning: {self.file_path} doesn't exist !"
+            )
+            return
         self.file_size = Path(file_path).stat().st_size
         if self.file_size != 0: self.empty = False
         else:   self.empty = True
         #* Warn if file doesn't exist
-        if not self.exists:
-            print(
-                f"Warning: {self.file_path} doesn't exist !"
-            )
     def file_missing (self) -> None:
         """
         Check if file is missing
@@ -47,7 +65,7 @@ class Json:
         """
         self.exists = Path(self.file_path).exists()
         self.file_name = Path(self.file_path).name
-        self.file_size = Path(file_path).stat().st_size
+        self.file_size = Path(self.file_path).stat().st_size
         if self.file_size != 0: self.empty = False
         else:   self.empty = True
     
@@ -57,7 +75,7 @@ class Json:
         Arguments: None
         """
         self.file_exist()
-        with open(self.file_path, "w") as json_file:
+        with open(self.file_path, "w", encoding = self.encoding) as json_file:
             dump({}, json_file)
         self.update()
         return True
@@ -76,7 +94,7 @@ class Json:
         Arguments: key: str
         """
         self.file_missing()
-        with open(self.file_path) as json_file:
+        with open(self.file_path, encoding = self.encoding) as json_file:
             raw = load(json_file)
         if key in raw:
             return True
@@ -90,7 +108,7 @@ class Json:
         """
 
         self.file_missing()
-        with open(self.file_path) as json_file:
+        with open(self.file_path, encoding = self.encoding) as json_file:
             raw = load(json_file)
         if value in raw.values():
             return True
@@ -102,7 +120,7 @@ class Json:
         Arguments: *optional* key: str
         """
         self.file_missing()
-        with open(self.file_path) as json_file:
+        with open(self.file_path, encoding = self.encoding) as json_file:
             raw = load(json_file)
         if key:
             return raw[key]
@@ -114,7 +132,7 @@ class Json:
         Arguments: value: any, *optional* key: str
         """
         self.file_missing()
-        with open(self.file_path, "r") as json_file:
+        with open(self.file_path, "r", encoding = self.encoding) as json_file:
             raw = load(json_file)
         if key:
             raw[key] = value
@@ -123,7 +141,7 @@ class Json:
             raw[len(raw)] = value
             self.update()
 
-        with open(self.file_path, "w") as json_file:
+        with open(self.file_path, "w", encoding = self.encoding) as json_file:
             dump(raw, json_file)
         return True
     
@@ -133,10 +151,10 @@ class Json:
         Arguments: key: str
         """
         self.file_missing()
-        with open(self.file_path, "r") as json_file:
+        with open(self.file_path, "r", encoding = self.encoding) as json_file:
             raw = load(json_file)
         raw.pop(key)
-        with open(self.file_path, "w") as json_file:
+        with open(self.file_path, "w", encoding = self.encoding) as json_file:
             dump(raw, json_file)
         self.update()
         return True
@@ -146,10 +164,10 @@ class Json:
         Delete value from json file
         Arguments: key: str
         """
-        with open(self.file_path, "r") as json_file:
+        with open(self.file_path, "r", encoding = self.encoding) as json_file:
             raw = load(json_file)
         raw[key] = None
-        with open(self.file_path, "w") as json_file:
+        with open(self.file_path, "w", encoding = self.encoding) as json_file:
             dump(raw, json_file)
             self.update()
         return True
@@ -160,10 +178,10 @@ class Json:
         Arguments: old_key: str, new_key: str
         """
         self.file_missing()
-        with open(self.file_path, "r") as json_file:
+        with open(self.file_path, "r", encoding = self.encoding) as json_file:
             raw = load(json_file)
         raw[new_key] = raw.pop(old_key)
-        with open(self.file_path, "w") as json_file:
+        with open(self.file_path, "w", encoding = self.encoding) as json_file:
             dump(raw, json_file)
             self.update()
         return True
@@ -174,13 +192,13 @@ class Json:
         Arguments: key: str, new_value: any
         """
         self.file_missing()
-        with open(self.file_path, "r") as json_file:
+        with open(self.file_path, "r", encoding = self.encoding) as json_file:
             raw = load(json_file)
         if new_value:
             raw[key] = new_value
         else:
             raw[key] = None
-        with open(self.file_path, "w") as json_file:
+        with open(self.file_path, "w", encoding = self.encoding) as json_file:
             dump(raw, json_file)
         self.update()
         return True
@@ -191,7 +209,7 @@ class Json:
         Arguments: None
         """
         self.file_missing()
-        with open(self.file_path, "w") as json_file:
+        with open(self.file_path, "w", encoding = self.encoding) as json_file:
             dump({}, json_file)
         self.update()
         return True
@@ -202,7 +220,7 @@ class Json:
         Arguments: None
         """
         self.file_missing()
-        with open(self.file_path) as json_file:
+        with open(self.file_path, encoding = self.encoding) as json_file:
             raw = load(json_file)
         return list(raw.keys())
     
@@ -216,7 +234,7 @@ class Json:
                 f"Sorry, {self.file_path} doesn't exist, create the file first, then try again."
             )
             return False
-        with open(self.file_path) as json_file:
+        with open(self.file_path, encoding = self.encoding) as json_file:
             raw = load(json_file)
         return list(raw.values())
     
@@ -226,6 +244,10 @@ class Json:
         Arguments: None
         """
         self.file_missing()
-        with open(self.file_path) as json_file:
+        with open(self.file_path, encoding = self.encoding) as json_file:
             raw = load(json_file)
         return list(raw.items())
+"""
+Credit: Akako0 https://github.com/Akako0/ez_json
+This code is open source and can be modified as wanted.
+"""
